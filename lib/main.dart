d74dd17bf77c2eefee4f94495bda6e0ff741d84e
hiren <hirenvadher954@gmail.com>
2022-01-19 21:21:59 +0530

    updating main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-import 'global.dart';
+import 'routes.dart';
+import 'views/views.dart';
 
-void main() => Global.init().then((e) => runApp(const MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyContactsApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyContactsApp extends StatelessWidget {
+  const MyContactsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: RootScreen.pageId,
+      getPages: appPages,
+    );
   }
 }
